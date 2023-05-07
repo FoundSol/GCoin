a8815a462ebf31b1362d7f5dfaf4bc254b1eee37
Ming Zhu <zhuming0828@fb.com>
2023-05-07 17:45:53 -0400

    change GCoin main contract to reflect latest requirement

--- contracts/GCoin.sol
+++ contracts/GCoin.sol
@@ -1,60 +1,53 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract GCoin is ERC20, Ownable {
+contract GCoin is ERC20, Ownable, Pausable {
 
     using SafeMath for uint256;
-
-    AggregatorV3Interface internal stableCoinPriceFeed;
-    ERC20 public stableCoin;
+    using SafeERC20 for ERC20;
 
     uint256 public mintingFee = 1; // 1% fee
-    uint256 public stableCoinDecimals;
     uint256 public gcoinValue;
-    address constant ETHER = address(0);
 
-    mapping(address => AggregatorV3Interface) public priceFeeds;
-    mapping(address => uint256) public discountFactors;
+    AggregatorV3Interface public gcoinPriceFeed;
+    mapping(address => ERC20) public stableCoins;
 
-    constructor(address _stableCoin, address _stableCoinPriceFeed)
-        ERC20("GCoin", "GC")
-    {
-        stableCoin = ERC20(_stableCoin);
-        stableCoinPriceFeed = AggregatorV3Interface(_stableCoinPriceFeed);
-        stableCoinDecimals = 10**uint256(stableCoin.decimals());
-        gcoinValue = stableCoinDecimals; // Initial value set to 1 stable coin
+    address public treasury;
+
+    constructor() ERC20("GCoin", "GC") {
+        gcoinValue = 1e18; // Initial value set to 1 stable coin
     }
 
-    function addTokenPriceFeed(address token, address priceFeed)
-        public
-        onlyOwner
-    {
-        priceFeeds[token] = AggregatorV3Interface(priceFeed);
+    function addStableCoin(address token) public onlyOwner {
+        stableCoins[token] = ERC20(token);
     }
 
-    function setDiscountFactor(address token, uint256 discount)
-        public
-        onlyOwner
-    {
-        discountFactors[token] = discount;
+    function setGCoinPriceFeed(address priceFeed) public onlyOwner {
+        gcoinPriceFeed = AggregatorV3Interface(priceFeed);
     }
 
-    function getLatestPrice(address token) private view returns (uint256) {
-        AggregatorV3Interface priceFeed = priceFeeds[token];
+    function setTreasury(address _treasury) public onlyOwner {
+        treasury = _treasury;
+    }
+
+    function getLatestPrice() private view returns (uint256) {
         require(
-            address(priceFeed) != address(0),
-            "Price feed not found for token"
+            address(gcoinPriceFeed) != address(0),
+            "GCoin price feed not set"
         );
-        (, int256 price, , , ) = priceFeed.latestRoundData();
+        (, int256 price, , , ) = gcoinPriceFeed.latestRoundData();
         return uint256(price);
     }
 
-    function getGCoinOutputFromStable(uint256 usdcAmount) public view returns (uint256) {
-        uint256 gcoinAmount = usdcAmount
+    function getGCoinOutputFromStable(address token, uint256 amount) public view returns (uint256) {
+        uint256 stableCoinDecimals = 10**uint256(stableCoins[token].decimals());
+        uint256 gcoinAmount = amount
             .mul(stableCoinDecimals)
             .div(gcoinValue)
             .mul(100)
@@ -62,72 +55,38 @@ contract GCoin is ERC20, Ownable {
         return gcoinAmount;
     }
 
-    function stableCoinToGCoin(uint256 amount) public {
+    function stableCoinToGCoin(address token, uint256 amount) public whenNotPaused {
+        ERC20 stableCoin = stableCoins[token];
+        require(address(stableCoin) != address(0), "Stable coin not found");
+        require(treasury != address(0), "Treasury address not set");
+
         uint256 allowance = stableCoin.allowance(msg.sender, address(this));
         require(allowance >= amount, "Amount not allowed");
 
+        uint256 stableCoinDecimals = 10**uint256(stableCoin.decimals());
         uint256 gcoinAmount = amount
             .mul(stableCoinDecimals)
             .div(gcoinValue)
             .mul(100)
             .div(100 + mintingFee);
-        stableCoin.transferFrom(msg.sender, address(this), amount);
+
+        uint256 initialTreasuryBalance = stableCoin.balanceOf(treasury);
+        stableCoin.safeTransferFrom(msg.sender, treasury, amount);
+        uint256 finalTreasuryBalance = stableCoin.balanceOf(treasury);
+
+        require(
+            finalTreasuryBalance == initialTreasuryBalance.add(amount),
+            "Treasury balance did not increase correctly"
+        );
+
         _mint(msg.sender, gcoinAmount);
     }
-
-    /*
-    This is really doesnt make sense, why people use other tokens to mint GCoin with 15% off?
-    If it is more like lending platform, then need to implement a bunch more to enable lending, redemption and liquidation.
-    */
-    function tokenToGCoin(uint256 amount, address token) public payable {
-        if (token == ETHER) {
-            uint256 ethAmount = msg.value;
-            uint256 ethPrice = getLatestPrice(ETHER);
-            uint256 ethAmountInStableCoin = ethAmount.mul(ethPrice).div(
-                stableCoinDecimals
-            );
-
-            uint256 discountedEthAmount = ethAmountInStableCoin
-                .mul(100 - discountFactors[ETHER])
-                .div(100);
-            uint256 gcoinAmount = discountedEthAmount
-                .mul(stableCoinDecimals)
-                .div(gcoinValue)
-                .mul(100)
-                .div(100 + mintingFee);
-            _mint(msg.sender, gcoinAmount);
-        } else {
-            uint256 allowance = ERC20(token).allowance(
-                msg.sender,
-                address(this)
-            );
-            require(allowance >= amount, "Amount not allowed");
-
-            uint256 tokenPrice = getLatestPrice(token);
-            uint256 tokenAmountInStableCoin = amount.mul(tokenPrice).div(
-                stableCoinDecimals
-            );
-
-            uint256 discountedTokenAmount = tokenAmountInStableCoin
-                .mul(100 - discountFactors[token])
-                .div(100);
-            uint256 gcoinAmount = discountedTokenAmount
-                .mul(stableCoinDecimals)
-                .div(gcoinValue)
-                .mul(100)
-                .div(100 + mintingFee);
-
-            ERC20(token).transferFrom(msg.sender, address(this), amount);
-            _mint(msg.sender, gcoinAmount);
-        }
-    }
-
     function getGCoinValue() public view returns (uint256) {
         return gcoinValue;
     }
 
     function updateGCoinValue() public onlyOwner {
-        uint256 chainlinkValue = getLatestPrice(address(stableCoin));
+        uint256 chainlinkValue = getLatestPrice();
         gcoinValue = chainlinkValue;
     }
 
@@ -137,5 +96,13 @@ contract GCoin is ERC20, Ownable {
 
     function burn(address from, uint256 amount) external onlyOwner {
         _burn(from, amount);
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
